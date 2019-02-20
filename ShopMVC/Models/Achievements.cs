f815adce7acdaa85b61aec9195403492f174866d
947806342@qq.com <947806342@qq.com>
2019-02-20 09:05:16 +0800

    王禄超

---
+++ ShopMVC/Models/Achievements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Models
+{
+   public class Achievements   //员工业绩表
+    {
+        public int AId { get; set; }  //主键
+        public int AEId { get; set; } //外键，关联员工信息表的主键
+        public int AMonth { get; set; }  //月度
+        public float ASale { get; set; }  //销售额
+    }
+}
---
+++ ShopMVC/Models/Market.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Models
+{
+   public class Market  //订单表
+    {
+        public int MId { get; set; }  //主键
+        public string MCard { get; set; }  //订单编号
+        public int MNnum { get; set; }   //购买数量
+        public int ShopId { get; set; }  //关联商品信息表主键
+    }
+}
