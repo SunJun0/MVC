0233434f25aa726efb459ae6ced6c8dd766b61ce
1825334857@qq.com <1825334857@qq.com>
2019-02-21 13:36:55 +0800

    Dong

---
+++ ShopMVC/Controllers/AchievementsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopMVC.Controllers
+{
+    public class AchievementsController : Controller
+    {
+        // GET: Achievements
+        //销售排行
+        public ActionResult GetAchievements()
+        {
+            return View();
+        }
+        public ActionResult Zhuye()
+        {
+            return View();
+        }
+    }
+}
