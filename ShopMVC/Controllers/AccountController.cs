f37f5d8505546c026ed3224cb14cfd8c1093d71e
Jane <1223383600@qq.com>
2019-02-26 16:52:37 +0800

    李伟

---
+++ ShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using ShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Login(string name, string pwd)
+        //{
+        //    string result = GetApiResult.Use("get", "Login/?name=" + name + "&pwd=" + pwd, "http://localhost:55237/api/account/");
+        //    Employees e = JsonConvert.DeserializeObject<Employees>(result);
+        //    if (e != null)
+        //    {
+        //        Session["employee"] = e;
+        //        return Content("1");
+        //    }
+        //    else
+        //    {
+        //        return Content("0");
+        //    }
+        //}
+    }
+}
