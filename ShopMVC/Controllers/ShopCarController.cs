195da1b1435d7324ed43e4affbf44fd43fc88560
巩明璇 <@DESKTOP-KSV5A8M>
2019-03-01 08:54:03 +0800

    合并

--- ShopMVC/Controllers/ShopCarController.cs
+++ ShopMVC/Controllers/ShopCarController.cs
@@ -13,6 +13,7 @@ namespace ShopMVC.Controllers
 {
     public class ShopCarController : Controller
     {
+        #region 商品查询
         static List<Goods> list = new List<Goods>();
 
         string dt = "";
@@ -21,7 +22,7 @@ namespace ShopMVC.Controllers
         public ActionResult ShopIndex()
         {
             ShopName();
-            if(num==1)
+            if (num == 1)
             {
                 num = list.Count;
                 ViewBag.sum = num;
@@ -30,34 +31,40 @@ namespace ShopMVC.Controllers
             {
                 ViewBag.sum = 0;
             }
-           
+
             return View();
         }
         [HttpPost]
         public ActionResult ShopIndex(Goods Shop)
         {
             ShopName();
-            if (Shop.GName == null & Shop.GColor == null & Shop.GSize == 0  )
+            if (Shop.GName == null & Shop.GColor == null & Shop.GSize == 0)
             {
                 Response.Write("<script>alert('请填写你要查询的商品')</script>");
             }
             else
             {
-                 dt = HttpClientHelper.Send("get", "/api/ShopCar/DemandShop/?ShopName=" + Shop.GName + "&ShopColor=" + Shop.GColor + "&ShopSize=" + Shop.GSize, null);
+                dt = HttpClientHelper.Send("get", "/api/ShopCar/DemandShop/?ShopName=" + Shop.GName + "&ShopColor=" + Shop.GColor + "&ShopSize=" + Shop.GSize, null);
 
                 list = JsonConvert.DeserializeObject<List<Goods>>(dt);
                 ViewBag.sum = list.Count;
             }
             return View(list);
         }
-        public  void ShopName()
+        #endregion
+
+        #region 绑定下拉（姓名）
+        public void ShopName()
         {
-            string sn =  HttpClientHelper.Send("get", "/api/ShopCar/ShopName", null);
+            string sn = HttpClientHelper.Send("get", "/api/ShopCar/ShopName", null);
 
             List<Goods> list = JsonConvert.DeserializeObject<List<Goods>>(sn);
 
             ViewBag.Gid = new SelectList(list, "GName", "GName");
         }
+        #endregion
+
+        #region 添加购物车
         public void AddShopCar(int num)
         {
 
@@ -65,24 +72,27 @@ namespace ShopMVC.Controllers
 
             shop.SNum = num;
 
-            shop.SState =Convert.ToBoolean(0);
+            shop.SState = Convert.ToBoolean(0);
 
             shop.GId = list[0].GId;
 
 
 
-            int i =Convert.ToInt32(GetApiResult.Use("post", "AddShopCar", "http://localhost:55237//api/ShopCar/", shop));
+            int i = Convert.ToInt32(GetApiResult.Use("post", "AddShopCar", "http://localhost:55237//api/ShopCar/", shop));
 
-            if(i>0)
+            if (i > 0)
             {
                 num = 0;
                 Response.Write("<script>alert('添加成功');location.href='/ShopCar/ShopIndex'</script>");
             }
         }
+        #endregion
+
+        #region 显示购物车
         static List<Shopping> shoplist = new List<Shopping>();
         public ActionResult ShowShopCar()
         {
-            string sj =  GetApiResult.Use("get", "ShowShopCar", "http://localhost:55237//api/ShopCar/");
+            string sj = GetApiResult.Use("get", "ShowShopCar", "http://localhost:55237//api/ShopCar/");
 
             shoplist = JsonConvert.DeserializeObject<List<Shopping>>(sj);
 
@@ -92,19 +102,25 @@ namespace ShopMVC.Controllers
                 Sum += shoplist[i].SNum * shoplist[i].GPricing;
             }
             ViewBag.Sum = Sum;
+            Session["Sum"] = Sum;
             return View(shoplist);
         }
-        List<Market> Mlist = new List<Market>();
-        int j = 0;
-        int t = 0;
-        public int TallyOrder(string Phone)
-        {
-            string dt = GetApiResult.Use("get", "ShowPhone/?Phone="+Phone, "http://localhost:55237//api/ShopCar/");
+        #endregion
 
-            List<Customer> CList = JsonConvert.DeserializeObject<List<Customer>>(dt);
-            if(CList.Count()>0)
-            {
-                Market Mmodel = new Market();
+        /// <summary>
+        /// 修改购物车状态
+        /// 修改商品库存
+        /// 添加订单
+        /// 修改顾客积分
+        /// 修改员工的业绩
+        /// 判断是否为vip用户
+        /// </summary>
+        List<Market> Mlist = new List<Market>();      
+        public void TallyOrder(string Phone)
+        {
+            int j = 0;
+            int t = 0;
+            Market Mmodel = new Market();
             Shopping shop = new Shopping();
             string time = DateTime.Now.ToString("MMddhhmm");
 
@@ -116,39 +132,47 @@ namespace ShopMVC.Controllers
                 Mmodel.MTime = DateTime.Now;
                 Mmodel.ShopId = shoplist[i].GId;
                 shop.SId = shoplist[i].SId;
-                j = Convert.ToInt32(GetApiResult.Use("post", "AddTallyOrder", "http://localhost:55237//api/ShopCar/",Mmodel));
-                t = Convert.ToInt32(GetApiResult.Use("put", "UpdateShopCar", "http://localhost:55237//api/ShopCar/", shop));
-                t += 1;
-                j += 1;
+                //提交订单
+                j += Convert.ToInt32(GetApiResult.Use("post", "AddTallyOrder", "http://localhost:55237//api/ShopCar/", Mmodel));
+                //修改购物车状态
+                t += Convert.ToInt32(GetApiResult.Use("put", "UpdateShopCar", "http://localhost:55237//api/ShopCar/", shop));
+
             }
-            if(t==shoplist.Count() && j==shoplist.Count())
+            if (t == shoplist.Count() && j == shoplist.Count())
             {
-                    int s = 0;
-                    Goods goods = new Goods();
-                    for (int i = 0; i < shoplist.Count(); i++)
-                    {
-                        goods.GId = shoplist[i].GId;
-                        goods.GQuantity = shoplist[i].SNum;
-                        s += Convert.ToInt32(GetApiResult.Use("put", "UpdateKu", "http://localhost:55237//api/ShopCar/", goods));
-                    }
-                    if (s == shoplist.Count())
-                    {
-                        Response.Write("成功");
-                    }
+                int s = 0;
+                Goods goods = new Goods();
+                for (int i = 0; i < shoplist.Count(); i++)
+                {
+                    goods.GId = shoplist[i].GId;
+                    goods.GQuantity = shoplist[i].SNum;
+                    //修改库存
+                    s += Convert.ToInt32(GetApiResult.Use("put", "UpdateKu", "http://localhost:55237//api/ShopCar/", goods));
                 }
 
-                //model m = new model();
-                //m.YId = 0;
-                //m.GId= CList[0].CId;
-                //m.SumPrice = ViewBag.Sum;
-                //int d =  Convert.ToInt32(GetApiResult.Use("put", "UpdateJi", "http://localhost:55237//api/ShopCar/", m));
-                
+                string dt = GetApiResult.Use("get", "ShowPhone/?Phone=" + Phone, "http://localhost:55237//api/ShopCar/");
+
+                List<Customer> CList = JsonConvert.DeserializeObject<List<Customer>>(dt);
+                model m = new model();
+                m.YId = 3;
+                m.SumPrice =Convert.ToSingle(Session["Sum"]);
+                int d = 0;
+                //判断是否为vip用户
+                if (CList.Count() > 0)
+                {
+                    m.GId = CList[0].CId;
+                    d = Convert.ToInt32(GetApiResult.Use("put", "UpdateJi", "http://localhost:55237//api/ShopCar/", m));
+                }
+                else
+                {
+                    d = Convert.ToInt32(GetApiResult.Use("put", "UpdateJi", "http://localhost:55237//api/ShopCar/", m));
+                }
+                if (s == shoplist.Count() && d >0)
+                {
+                    Response.Write("<script>alert('交易完成')</script>");
+                }
 
             }
-
-
-
-            return 1;
         }
     }
 }
