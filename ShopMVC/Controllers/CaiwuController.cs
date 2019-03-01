8ffa4f3e7dab6ccdf8f29c98af82ba0f323697bb
947806342@qq.com <947806342@qq.com>
2019-03-01 13:38:21 +0800

    w

--- ShopMVC/Controllers/CaiwuController.cs
+++ ShopMVC/Controllers/CaiwuController.cs
@@ -39,5 +39,22 @@ namespace ShopMVC.Controllers
             //ViewBag.ss = jias;
             return View();
         }
+        //导出
+        public ActionResult Export()
+        {
+            /*指定Excel行与集合中对象的对应关系*/
+            Dictionary<string, string> dts = new Dictionary<string, string>
+            {
+            {"RowNum","员工排行"},
+            {"EName","员工名称"},
+            {"ESex","员工性别"},
+            {"payroll","员工工资"},
+            };
+            string result = HttpClientHelper.Send("get", "/api/Employees/ShowWages", null);
+            List<Achievements> list = JsonConvert.DeserializeObject<List<Achievements>>(result);
+            //调用ExcelHelper，将返回的路径配置新的URl路由规则
+            string url = Url.RouteUrl("Download") + ExcelHelper.EntityListToExcel2003(dts, list, "工资表");
+            return Content("<script>location.href='" + url + "'</script>");
+        }
     }
 }
